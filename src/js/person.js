0267b6f248204ea5523168139d80b91d5b2d6c5b
piavw <piavonwachenfelt@hotmail.com>
2019-08-28 14:46:57 +0200

    Created features, src/index.html and solved misspelling which caused the test to fail. 6 out of 6 test passing.

--- src/js/person.js
+++ src/js/person.js
@@ -1,12 +1,12 @@
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Person;
+}
+
 function Person(attr) {
     this.weight = attr.weight;
     this.height = attr.height;
     this.calculate_bmi = function() {
-        this.bmiValue = 26.01;
-        this.bmiMessage = "Overweight"
-    }
-};
-
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = Person;
-} 
+        calculator = new BMICalculator();
+        calculator.metric_bmi(this)
+    };
+}
