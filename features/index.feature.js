dcfd0aaa4ec8810c4ce7da576d386880aa9af61b
piavw <piavonwachenfelt@hotmail.com>
2019-08-28 17:25:37 +0200

    Debugged and solved error in index.html which caused functiontests to not pass. 6/6 spectest and functiontest passing.

--- features/index.feature.js
+++ features/index.feature.js
@@ -13,13 +13,14 @@ describe('User can calculate BMI with the metric units', () => {
     after(async () => {
         await browser.close();
     });
-    it('by inputing his weight and height', async () => {
-    await browser.fillIn("input[id='weight-in-kgs']", { with: "95"})
-    await browser.fillIn("input[id='height-in-cm']", { with: "185"})
 
-    await browser.clickOnButton("button")
-    let content = await browser.getContent("span[id='display_value']")
-    expect(content).to.eql('Your BMI is 27.76');
+    it('by inputing his weight and height', async () => {
+        await browser.fillIn("input[id='weight-in-kgs']", { with: "95"})
+        await browser.fillIn("input[id='height-in-cm']", { with: "185"})
+        await browser.clickOnButton("button")
+
+        let content = await browser.getContent("span[id='display_value']")
+        expect(content).to.eql('Your BMI is 27.76');
     });
 });
 
@@ -34,13 +35,14 @@ describe('User can calculate BMI using imperial system', () => {
     after(async () => {
         await browser.close();
     });
+
     it('by inputing his weight and height', async () => {
         await browser.selectOption("select[id='my_select']", {option: 'imperial' })
         await browser.fillIn("input[id='weight-in-lbs']", { with: "170"})
         await browser.fillIn("input[id='height-in-in']", { with: "70"})
         await browser.clickOnButton("button")
-        let content = await browser.getContent("span[id='display_value']")
 
-        expect (content).to.eql('Your BMI is 24.39')
+        let content = await browser.getContent("span[id='display_value']")
+        expect(content).to.eql('Your BMI is 24.39');
     });
 });
--- src/js/person.js
+++ src/js/person.js
@@ -1,17 +1,17 @@
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = Person;
-}
-
 function Person(attr) {
     this.weight = attr.weight;
     this.height = attr.height;
     this.calculate_bmi = function() {
         calculator = new BMICalculator();
         calculator.metric_bmi(this)
+    }
+    Person.prototype.calculate_imperial_bmi = function() {
+        calculator = new BMICalculator();
+        calculator.imperial_bmi(this);
     };
+};
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Person;
 }
 
-Person.prototype.calculate_imperial_bmi = function() {
-    calculator = new BMICalculator();
-    calculator.imperial_bmi(this);
-};
